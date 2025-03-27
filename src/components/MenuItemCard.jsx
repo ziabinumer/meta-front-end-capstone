dd77b0b2001f00fa7eb15e07c00c726e3b84c44d
= <ziabinumer@gmail.com>
2025-03-27 13:01:17 +0500

    add

---
+++ src/components/MenuItemCard.jsx
@@ -0,0 +1,26 @@
+import Card from "react-bootstrap/Card"
+import Container from "react-bootstrap/Container"
+import Button from "react-bootstrap/Button"
+
+export default function MenuItemCard(props) {
+    const item = props.item
+    return (
+            <Card style={{width: "22rem", maxHeight: "450px"}}>
+                <Card.Img variant="top" src={item.src}/>
+                <Card.Body>
+                    <Card.Title className="d-flex flex-row justify-content-between">
+                        <span>{item.name}</span>
+                        <span className="text-danger" style={{marginRight:"8px"}}>{item.price}</span>
+                    </Card.Title>
+                    <Card.Text>
+                        {item.text}
+                    </Card.Text>
+                    <Container className="w-100 d-flex justify-content-center">
+                        <Button variant="primary">
+                            Order Online
+                        </Button>
+                    </Container>
+                </Card.Body>
+            </Card>
+    )
+}
