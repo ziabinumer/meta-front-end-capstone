7bdb808e1e2567ddfb84cff1339ab64bdcf9d874
= <ziabinumer@gmail.com>
2025-04-03 18:31:32 +0500

    improve

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,66 @@
+import Image from "react-bootstrap/Image";
+import ListGroup from "react-bootstrap/ListGroup";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {Link} from "react-router-dom"
+
+import logo from "../../assets/logo-white.png";
+import { NavLinks, SocialLinks, ContactDetail } from "./Links";
+
 export default function Footer() {
     return (
-        <></>
-    )
+        <footer className="text-light py-5" style={{ backgroundColor: "#495E57" }}>
+            <Container>
+                <Row className="gy-4 align-items-center text-center text-md-start">
+                    
+                    {/* Logo Section */}
+                    <Col xs={12} md={3} className="d-flex justify-content-center justify-content-md-start">
+                        <Image src={logo} className="me-md-4" style={{ width: "100px" }} />
+                    </Col>
+
+                    {/* Sitemap Section */}
+                    <Col xs={12} md={3}>
+                        <h5 className="fw-bold">Sitemap</h5>
+                        <ListGroup className="list-unstyled">
+                            {NavLinks.map((link, index) => (
+                                <Link to={link.add} className="navLink">
+                                    {link.name}
+                                </Link>
+                            ))}
+                        </ListGroup>
+                    </Col>
+
+                    {/* Contact Details */}
+                    <Col xs={12} md={3}>
+                        <h5 className="fw-bold">Contact</h5>
+                        <ListGroup className="list-unstyled">
+                            {ContactDetail.map((item, index) => (
+                                <ListGroup.Item key={index} className="bg-transparent border-0 text-light d-flex align-items-center justify-content-center justify-content-md-start">
+                                    <FontAwesomeIcon icon={item.icon} className="me-3" />
+                                    <span>{item.name}</span>
+                                </ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                    </Col>
+
+                    {/* Social Links */}
+                    <Col xs={12} md={3}>
+                        <h5 className="fw-bold">Follow Us</h5>
+                        <ListGroup horizontal className="justify-content-center justify-content-md-start">
+                            {SocialLinks.map((link, index) => (
+                                <ListGroup.Item key={index} className="bg-transparent border-0">
+                                    <a href={link.path} className="text-light text-decoration-none">
+                                        <FontAwesomeIcon icon={link.icon} size="lg" />
+                                    </a>
+                                </ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                    </Col>
+                    
+                </Row>
+            </Container>
+        </footer>
+    );
 }
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,53 +1,43 @@
-import Container from "react-bootstrap/Container"
-import Nav from "react-bootstrap/Nav"
-import Navbar from "react-bootstrap/Navbar"
-import Image from "react-bootstrap/Image"
-
-import logo from "../../assets/logo.png"
-
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Image from "react-bootstrap/Image";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import { Link } from "react-router-dom";
 
-
-//import "bootstrap/dist/css/bootstrap.min.css"
+import logo from "../../assets/logo.png";
+import { NavLinks as links } from "./Links";
 
 export default function Header() {
-    const links = [
-        {
-            name: "Home",
-            add: "/",
-        },
-        {
-            name: "About",
-            add: "/about"
-        },
-        {
-            name: "Menu",
-            add: "/menu"
-        },
-        {
-            name: "Reservation",
-            add: "/reservation"
-        },
-        {
-            name: "Order",
-            add: "/order"
-        },
-        {
-            name: "Login",
-            add: "login"
-        }
-    ]
     return (
-        <div id="Header">
-            <Navbar style={{backgroundColor: "white"}} data-bs-theme="light" className="py-3">
+        <header>
+            <Navbar expand="lg" style={{ backgroundColor: "white" }} data-bs-theme="light" className="py-3">
                 <Container>
-                    <Navbar.Brand href="/" className="w-25">
-                        <Image src={logo} className="" style={{ width: "50%"}}/>
+                    <Navbar.Brand as={Link} to="/" className="w-25">
+                        <Image src={logo} style={{ width: "50%" }} alt="Logo" />
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
+                    
+                    <Navbar.Toggle aria-controls="offcanvasNavbar" className="d-lg-none" />
+
+                    <Navbar.Offcanvas id="offcanvasNavbar" placement="start" className="d-lg-none">
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title>Menu</Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="d-flex flex-column">
+                                {links.map((link, index) => (
+                                    <Nav.Link as={Link} to={link.add} key={index} className="navLink">
+                                        {link.name}
+                                    </Nav.Link>
+                                ))}
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+
+                    {/* Desktop menu*/}
+                    <Navbar.Collapse id="basic-navbar-nav" className="d-none d-lg-flex">
                         <Nav className="w-75 d-flex justify-content-evenly">
-                            {links.map((link, index)=> (
+                            {links.map((link, index) => (
                                 <Nav.Link as={Link} to={link.add} key={index} className="navLink">
                                     {link.name}
                                 </Nav.Link>
@@ -56,6 +46,6 @@ export default function Header() {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-        </div>
-    )
+        </header>
+    );
 }
