4c5b62d1e32999451ee96638feb9da6c01b1abf3
= <ziabinumer@gmail.com>
2025-04-03 18:14:52 +0500

    push from local

---
+++ src/components/pages/NotFound/index.jsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+        <section className="d-flex justify-content-center align-items-center w-100 h-100" id="MessagePage">
+            <h1 className="color-primary">
+                Not Found
+            </h1>
+        </section>
+    )
+}
