90eab721d13f933f9b0b568ff214839624c16801
= <ziabinumer@gmail.com>
2025-04-03 18:16:33 +0500

    implement

---
+++ src/components/pages/ComingSoon/index.jsx
@@ -0,0 +1,9 @@
+export default function ComingSoon() {
+    return (
+        <section className="d-flex justify-content-center align-items-center w-100 h-100" id="MessagePage">
+            <h1 className="color-primary">
+                Coming Soon
+            </h1>
+        </section>
+    )
+}
